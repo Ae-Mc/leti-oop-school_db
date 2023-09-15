0c3b8e542a8c154c59631c14d7c96ef13145c5a9
Ae-Mc <ae_mc@mail.ru>
2023-09-15 23:54:47 +0300

    Добавил метод addMark для класса Student

--- src/jvmMain/kotlin/entities/Student.kt
+++ src/jvmMain/kotlin/entities/Student.kt
@@ -4,6 +4,7 @@ import org.jetbrains.exposed.dao.UUIDEntity
 import org.jetbrains.exposed.dao.UUIDEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.UUIDTable
+import java.time.LocalDate
 import java.util.*
 
 object Students : UUIDTable() {
@@ -17,4 +18,15 @@ class Student(id: EntityID<UUID>) : UUIDEntity(id) {
     var fullName by Students.fullName
     var studentClass by Students.studentClass
     val marks by Mark backReferencedOn Marks.student
+
+    fun addMark(teacher: Teacher, subject: Subject, mark: Int, date: LocalDate?): Mark {
+        val student = this
+        return Mark.new {
+            this.subject = subject
+            this.student = student
+            this.teacher = teacher
+            this.mark = mark
+            this.date = date ?: LocalDate.now()
+        }
+    }
 }
